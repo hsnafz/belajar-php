61d1c26c1925d265bc8701a24139c5c1da80f9a1
Hasna Hafizhah <hasnahafizhah11@gmail.com>
2022-10-24 19:08:19 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Hasna";
+$usia = 20;
+
+echo "Nama saya $nama, saya berusia $usia tahun.<br>";
+
+$namaAdik = "Afiya";
+$usiaAdik = 18;
+
+echo "Nama adik saya $namaAdik, dia berusia $usiaAdik tahun.<br>";
+
+$selisihUsia = $usia - $usiaAdik;
+
+echo "Selisih usia kami adalah $selisihUsia tahun."
+
+?>
